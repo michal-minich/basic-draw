5f094196d82882854ee9119a47f2a2a54ac3771d
Michal Minich <michal.minich@icloud.com>
2021-10-27 10:37:50 +0200

    JS drawing

---
+++ dist/web/drawings/drawing.js
@@ -0,0 +1,7 @@
+//@ts-check
+import { move, line } from "../scripts/App.js";
+
+
+move(0, 0)
+
+line(100, 100)
--- src/web/App.ts
+++ src/web/App.ts
@@ -1,11 +1,16 @@
 
-
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
 
-ctx.beginPath();       // Start a new path
-ctx.moveTo(30, 50);    // Move the pen to (30, 50)
-ctx.lineTo(150, 100);  // Draw a line to (150, 100)
-ctx.stroke();          // Render the path
 
-console.log("A");
+export function move(x: number, y: number): void {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+}
+
+
+export function line(x: number, y: number): void {
+
+    ctx.lineTo(x, y);
+    ctx.stroke();
+}
